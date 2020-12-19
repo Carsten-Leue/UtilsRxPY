abcb0e6766ea74fcc0cb99386c496dc68b05866d
Dr. Carsten Leue <carsten.leue@gmx.net>
2020-12-19 21:13:55 +0100

    Bump version: 0.0.2 → 0.0.3

--- rx_utils/_internal/__init__.py
+++ rx_utils/_internal/__init__.py
@@ -12,4 +12,4 @@
 """Implementation details"""
 
 # version information
-__version__ = '0.0.2'
+__version__ = '0.0.3'
--- rx_utils/docs/__init__.py
+++ rx_utils/docs/__init__.py
@@ -14,7 +14,7 @@ from typing import Sequence
 from os.path import dirname, join, normpath
 
 # version information
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 def get_objects_inv_path() -> str:
